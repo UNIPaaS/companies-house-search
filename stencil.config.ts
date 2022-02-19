c57bad981a0d0fd8d435cc4f405bd8064ff613e8
Ron Mizrahi <sronmiz@gmail.com>
2022-02-19 21:35:24 +0200

    wip

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,19 @@
 import { Config } from '@stencil/core';
+import { angularOutputTarget as angular } from '@stencil/angular-output-target';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'companies-house-component',
   outputTargets: [
+    angular({
+      componentCorePackage: `unipaas`,
+      directivesProxyFile: `../angular/src/lib/stencil-generated/components.ts`
+    }),
+    react({
+      componentCorePackage: 'unipaas',
+      proxiesFile: '../react/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
